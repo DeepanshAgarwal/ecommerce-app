a322fdab39b0eba1a6b414b24d471bf9b5302f05
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2024-11-15 01:44:18 +0530

    Implement user authentication with login and signup functionality; enhance Navbar with logout feature and token management

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -13,6 +13,7 @@ const ShopContextProvider = (props) => {
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
     const [products, setProducts] = useState([]);
+    const [token, setToken] = useState("");
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -89,6 +90,12 @@ const ShopContextProvider = (props) => {
         getProductsData();
     }, []);
 
+    useEffect(() => {
+        if (!token && localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"));
+        }
+    }, []);
+
     const value = {
         products,
         currency,
@@ -104,6 +111,8 @@ const ShopContextProvider = (props) => {
         getCartAmount,
         navigate,
         backendUrl,
+        token,
+        setToken,
     };
 
     return (
